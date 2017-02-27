354d11624643d0d2b6fd9a4c9537d8c93cf672e1
TheUnderTaker11 <cabmanbellou@hotmail.com>
2017-02-26 19:46:55 -0600

    Added hasEffect parameter to VCItem
    Make it so the item can look enchanted.

--- src/main/java/mdc/voodoocraft/items/VCItem.java
+++ src/main/java/mdc/voodoocraft/items/VCItem.java
@@ -3,13 +3,29 @@ package mdc.voodoocraft.items;
 import mdc.voodoocraft.VoodooCraft;
 import mdc.voodoocraft.util.Refs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class VCItem extends Item
 {
-    public VCItem(String name)
+	private boolean HasEffect;
+	
+    public VCItem(String name, boolean hasEffect)
     {
         setUnlocalizedName(Refs.MODID + name);
         setRegistryName(name);
         setCreativeTab(VoodooCraft.CREATIVE_TAB);
+    }
+    
+    public VCItem(String name)
+    {
+    	this(name, false);
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public boolean hasEffect(ItemStack stack)
+    {
+        return this.HasEffect;
     }
 }

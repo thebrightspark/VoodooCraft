5c9717a684c1adb0b8927f73894040dccca17f4a
bright_spark <markpassey@btinternet.com>
2017-02-27 02:31:51 +0000

    Fixed startup crash

--- src/main/java/mdc/voodoocraft/util/Refs.java
+++ src/main/java/mdc/voodoocraft/util/Refs.java
@@ -3,10 +3,10 @@ package mdc.voodoocraft.util;
 public class Refs {
 
     public static final String MODID = "voodoocraft";
-    public static final String NAME = "Voodoo Craft";
+    public static final String NAME = "VoodooCraft";
     public static final String VERSION = "1.10.2-0.0.1";
     public static final String AVERSION = "[1.10, 1.10.2]";
 
-    public static final String CPROXY = "mdc.voodoocraft.proxy.Client";
-    public static final String SPROXY = "mdc.voodoocraft.proxy.Common";
+    public static final String CPROXY = "mdc.voodoocraft.proxy.ClientProxy";
+    public static final String SPROXY = "mdc.voodoocraft.proxy.CommonProxy";
 }

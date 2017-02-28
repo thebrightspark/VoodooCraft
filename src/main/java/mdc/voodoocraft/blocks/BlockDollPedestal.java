bb699593d92f8dcc340dc0e4eed86c5d43485f95
TheUnderTaker11 <cabmanbellou@hotmail.com>
2017-02-28 11:31:49 -0600

    Last commit directly to main branch
    Added these last night, going to go ahead and commit it.

--- src/main/java/mdc/voodoocraft/blocks/BlockDollPedestal.java
+++ src/main/java/mdc/voodoocraft/blocks/BlockDollPedestal.java
@@ -1,9 +1,18 @@
 package mdc.voodoocraft.blocks;
 
+import javax.annotation.Nullable;
+
+import mdc.voodoocraft.tile.TileDollPedestal;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
 
 public class BlockDollPedestal extends VCModelBlock{
 
@@ -11,11 +20,34 @@ public class BlockDollPedestal extends VCModelBlock{
 	
 	public BlockDollPedestal() {
 		super("dollpedestal");
+		this.isBlockContainer = true;
 	}
 
 	@Override
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
 		return hitbox;
 	}
+	
+	public TileEntity createTileEntity(World world, IBlockState state)
+    {
+		return new TileDollPedestal();
+    }
+	@Override
+	public boolean hasTileEntity(IBlockState state){return true;}
+	
+	@Override
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ)
+    {
+		if(hand==EnumHand.OFF_HAND||worldIn.getTileEntity(pos)==null) return false;
+		
+		TileDollPedestal tile = (TileDollPedestal)worldIn.getTileEntity(pos);
+		if(playerIn.getHeldItem(EnumHand.MAIN_HAND)==null)
+		{
+			//Take item out pedestal
+		}else{
+			//Put item in the pedestal
+		}
+        return false;
+    }
 	
 }
---
+++ src/main/java/mdc/voodoocraft/tile/TileDollPedestal.java
@@ -0,0 +1,62 @@
+package mdc.voodoocraft.tile;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+
+public class TileDollPedestal extends TileEntity implements ITickable{
+	
+	public TileDollPedestal(){super();}
+
+	@Override
+	public void update()
+	{
+		//Tick doll
+	}
+	
+    private ItemStackHandler itemStackHandler = 
+    		new ItemStackHandler(1){
+        @Override
+        protected void onContentsChanged(int slot) {
+        	TileDollPedestal.this.markDirty();
+            
+        }
+    };
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+        if (compound.hasKey("items")) {
+            itemStackHandler.deserializeNBT((NBTTagCompound) compound.getTag("items"));
+        }
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound){
+        super.writeToNBT(compound);
+        compound.setTag("items", itemStackHandler.serializeNBT());
+        return compound;
+    }
+
+    @Override
+    public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return true;
+        }
+        return super.hasCapability(capability, facing);
+    }
+
+    @Override
+    public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
+        if (capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+            return (T) itemStackHandler;
+        }
+        return super.getCapability(capability, facing);
+    }
+
+}

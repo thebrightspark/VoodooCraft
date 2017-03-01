2cd3a5ad661e9787c60220c860171f0d020b5625
Unknown <j-schirra@gmx.de>
2017-03-01 13:04:21 +0100

    made the chalk symbol block final

--- src/main/java/mdc/voodoocraft/init/VCBlocks.java
+++ src/main/java/mdc/voodoocraft/init/VCBlocks.java
@@ -36,3 +36,33 @@ public class VCBlocks
         GameRegistry.registerTileEntity(teClass, loc.getResourceDomain() + "_" + loc.getResourcePath());
     }
 }
+
+import mdc.voodoocraft.blocks.BlockChalk;
+import mdc.voodoocraft.blocks.BlockDollPedestal;
+import net.minecraft.block.Block;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class VCBlocks
+{
+
+    public static Block CHALK_BASIC_SYMBOL = new BlockChalk("chalkbasicsymbol");
+    public static final Block DOLL_PEDESTAL = new BlockDollPedestal();
+
+    public static void registerTileEntities()
+    {
+        //Register Tile Entities
+    }
+    
+    public static void registerTileEntityRenders() {
+    	//register TESRs
+    }
+    
+    @SuppressWarnings("unused")
+	private static void regTE(Class<? extends TileEntity> teClass, Block block)
+    {
+    	ResourceLocation loc = block.getRegistryName();
+        GameRegistry.registerTileEntity(teClass, loc.getResourceDomain() + "_" + loc.getResourcePath());
+    }
+}

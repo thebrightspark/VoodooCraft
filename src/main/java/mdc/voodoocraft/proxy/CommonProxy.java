38421e49a1b8d3f4c8fb116f10a4fa1b6a1240b4
TheUnderTaker11 <cabmanbellou@hotmail.com>
2017-03-01 18:04:11 -0600

    Fixed bug
    My b

--- src/main/java/mdc/voodoocraft/proxy/CommonProxy.java
+++ src/main/java/mdc/voodoocraft/proxy/CommonProxy.java
@@ -1,46 +1,7 @@
-<<<<<<< UnderTaker-1.10.2
 package mdc.voodoocraft.proxy;
 
 import mdc.voodoocraft.config.VoodooConfig;
 import mdc.voodoocraft.init.VCBlocks;
-import mdc.voodoocraft.init.VCEntities;
-import mdc.voodoocraft.init.VCRecipes;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import net.minecraftforge.fml.common.event.FMLServerStoppingEvent;
-
-public class CommonProxy
-{
-
-    public void preInit(FMLPreInitializationEvent e)
-    {
-    	VoodooConfig.init(e.getSuggestedConfigurationFile());
-        VCEntities.init();
-    }
-
-    public void init(FMLInitializationEvent e)
-    {
-        VCRecipes.init();
-    }
-
-    public void postInit(FMLPostInitializationEvent e){
-    	
-    }
-    
-    public void serverStarting(FMLServerStartingEvent e) {
-    	//register commands
-    }
-    
-    public void serverStopping(FMLServerStoppingEvent e) {
-    	
-    }
-}
-=======
-package mdc.voodoocraft.proxy;
-
-import mdc.voodoocraft.config.VoodooConfig;
 import mdc.voodoocraft.init.VCEntities;
 import mdc.voodoocraft.init.VCRecipes;
 import mdc.voodoocraft.init.VCSoundHandler;
@@ -78,4 +39,3 @@ public class CommonProxy
     	
     }
 }
->>>>>>> master

378685b13d480872bdbaae5beaac1bedbb031188
Alex Couch <alcouch65@gmail.com>
2017-03-07 22:40:48 -0800

    Starting Command

--- src/main/java/mdc/voodoocraft/handlers/VCEventHandler.java
+++ src/main/java/mdc/voodoocraft/handlers/VCEventHandler.java
@@ -1,17 +1,21 @@
 package mdc.voodoocraft.handlers;
 
-import mdc.voodoocraft.blocks.BlockGlyph;
-import mdc.voodoocraft.init.VCBlocks;
-import mdc.voodoocraft.util.EnumGlyphType;
-import net.minecraft.block.material.Material;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraftforge.event.entity.living.LivingDeathEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
+import mdc.voodoocraft.blocks.*;
+import mdc.voodoocraft.init.*;
+import mdc.voodoocraft.util.*;
+import net.minecraft.block.material.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.player.*;
+import net.minecraft.util.math.*;
+import net.minecraft.util.text.*;
+import net.minecraft.world.*;
+import net.minecraftforge.event.entity.*;
+import net.minecraftforge.event.entity.living.*;
+import net.minecraftforge.fml.common.*;
+import net.minecraftforge.fml.common.Mod.*;
+import net.minecraftforge.fml.common.event.*;
+import net.minecraftforge.fml.common.eventhandler.*;
+import net.minecraftforge.fml.common.gameevent.*;
 
 @Mod.EventBusSubscriber
 public class VCEventHandler {
@@ -27,6 +31,19 @@ public class VCEventHandler {
         } else if (world.getBlockState(pos.down()).getMaterial() == Material.LAVA) {
             player.motionY = 0.1D;
         }
+    }
+    
+    @SubscribeEvent
+    public static void displayWelcomeChat(EntityJoinWorldEvent event){
+    	if(!event.getWorld().isRemote){
+			if (event.getEntity() instanceof EntityPlayerMP) {
+				EntityPlayerMP player = (EntityPlayerMP) event.getEntity();
+				player.sendMessage(new TextComponentString(TextFormatting.GOLD + "Welcome to VoodooCraft by ModDev-Cafe Modding Team!"));
+				player.sendMessage(new TextComponentString(TextFormatting.GOLD + "For Tips and Tricks About the Mod"));
+				player.sendMessage(new TextComponentString(TextFormatting.GOLD + "Use the Following Command: "));
+				player.sendMessage(new TextComponentString(TextFormatting.GOLD + "/vdcraft help <feature>"));
+			}
+    	}
     }
     
     @SubscribeEvent

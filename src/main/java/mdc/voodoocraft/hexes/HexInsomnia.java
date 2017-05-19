98063247ececb333605d48a0157d0f79b2c5535d
alexcouch <alcouch65@gmail.com>
2017-05-19 16:28:11 -0700

    Fixed bug that performs hexes twice due to code being ran on both server and client sides at the same time, making the hex debugging much easier.

--- src/main/java/mdc/voodoocraft/hexes/HexInsomnia.java
+++ src/main/java/mdc/voodoocraft/hexes/HexInsomnia.java
@@ -1,9 +1,12 @@
 package mdc.voodoocraft.hexes;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class HexInsomnia extends HexEntry {
 
@@ -12,11 +15,13 @@ public class HexInsomnia extends HexEntry {
     }
 
     @Override
-    public ItemStack activeUse(ItemStack stackIn, World world, EntityPlayer player, EnumHand hand, int strength) {
-        if (world.getWorldTime() >= 11615) {
-            world.setWorldTime(1000);
+    public ItemStack activeUse(ItemStack stackIn, World world, EntityPlayer player, EnumHand hand, int strength, @Nullable EntityLivingBase target) {
+        if(world.isRemote) {
+            if (world.getWorldTime() >= 11615) {
+                world.setWorldTime(1000);
+            }
         }
-        return super.activeUse(stackIn, world, player, hand, strength);
+        return super.activeUse(stackIn, world, player, hand, strength, target);
     }
 
 
--- src/main/java/mdc/voodoocraft/hexes/HexZombify.java
+++ src/main/java/mdc/voodoocraft/hexes/HexZombify.java
@@ -1,15 +1,21 @@
 package mdc.voodoocraft.hexes;
 
 
+import mdc.voodoocraft.VoodooCraft;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class HexZombify extends HexEntry {
     public HexZombify(){
@@ -17,19 +23,31 @@ public class HexZombify extends HexEntry {
     }
 
     @Override
-    public ItemStack activeUse(ItemStack stackIn, World world, EntityPlayer player, EnumHand hand, int strength) {
-        //Gets the result of the raytrace intersection that is 10 blocks away
-        RayTraceResult result = player.rayTrace(10, 1);
-        //Gets the entity from the raytrace result
-        Entity target = result.entityHit;
-        //Do what you need with the entity variable
-        EntityVillager villager = new EntityVillager(world);
-        if(target instanceof EntityZombie){
-            System.out.println("Hit the if *******************");
+    public ItemStack activeUse(ItemStack stackIn, World world, EntityPlayer player, EnumHand hand, int strength, @Nullable EntityLivingBase target) {
+        if(world.isRemote) {
+            //Gets the result of the raytrace intersection that is 10 blocks away
+            RayTraceResult result = Minecraft.getMinecraft().objectMouseOver;
+            //Gets the entity from the raytrace result
+            Entity e = result.entityHit;
+            //Do what you need with the entity variable
+            if (e != null) {
+                if (e instanceof EntityZombie) {
+                    EntityVillager villager = new EntityVillager(world);
+                    villager.copyLocationAndAnglesFrom(e);
+                    villager.onInitialSpawn(world.getDifficultyForLocation(new BlockPos(villager)), null);
+                    world.removeEntity(e);
+                    world.spawnEntity(villager);
+                    VoodooCraft.log.info("Changed zombie to villager...");
+                } else if (e instanceof EntityVillager) {
+                    EntityZombie zombie = new EntityZombie(world);
+                    zombie.copyLocationAndAnglesFrom(e);
+                    zombie.onInitialSpawn(world.getDifficultyForLocation(new BlockPos(zombie)), null);
+                    world.removeEntity(e);
+                    world.spawnEntity(zombie);
+                    VoodooCraft.log.info("Changed villager to zombie...");
+                }
+            }
         }
-
-
-
-        return super.activeUse(stackIn, world, player, hand, strength);
+        return super.activeUse(stackIn, world, player, hand, strength, target);
     }
 }
